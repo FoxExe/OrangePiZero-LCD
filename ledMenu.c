f659a6c5a82d1c4d1c7383e01d65875ee54b0cd5
Allester Fox <fox.axon@gmail.com>
2017-09-17 00:14:50 +0500

    Added function to show date and time

--- ledMenu.c
+++ ledMenu.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <signal.h>	// Ctrl+c handler
+#include <signal.h> // Ctrl+c handler
+#include <time.h>
 
 #include <wiringPi.h>
 #include "ledMenu.h"
@@ -25,7 +26,7 @@ void DoNothing()
 }
 
 static MenuItem MainMenu[] = {
-	{"Nothing :)", DoNothing},
+	{"Show time", ShowTime},
 	{"Backlight", PrintBackLightMenu},
 	{"Network", PrintNetworkSettings},
 };
@@ -49,12 +50,13 @@ Button Buttons[MAX_BTN] = {
 	{PIN_RIGHT, DoNothing},
 };
 
-int ActiveMenuItem = 1;	// SHould start from 1 for future tests
+int ActiveMenuItem = 1; // SHould start from 1 for future tests
 int ActiveMenuItems = 0;
 static int keepRunning = 1;
 MenuItem (*CurrentMenu)[];
 
-void intHandler(int dummy) {
+void intHandler(int dummy)
+{
 	printf("\nGot signal: %d, quit...\n", dummy);
 	keepRunning = 0;
 }
@@ -76,7 +78,7 @@ void DrawMenu()
 			LCDdrawstring(6, i * 8 + 1, (*CurrentMenu)[i].Name);
 		}
 		*/
-		LCDdrawstring(0, i * 8 + 1, (i == (ActiveMenuItem - 1))? ">" : " ");
+		LCDdrawstring(0, i * 8 + 1, (i == (ActiveMenuItem - 1)) ? ">" : " ");
 		LCDdrawstring(6, i * 8 + 1, (*CurrentMenu)[i].Name);
 	}
 	LCDdisplay();
@@ -109,9 +111,9 @@ void MenuDown()
 }
 
 void PrintMainMenu()
-{	
+{
 	Buttons[KEY_RIGHT].OnPress = RunSelected;
-	Buttons[KEY_LEFT].OnPress = PrintMainMenu;	// Return to top
+	Buttons[KEY_LEFT].OnPress = PrintMainMenu; // Return to top
 	Buttons[KEY_UP].OnPress = MenuUp;
 	Buttons[KEY_DOWN].OnPress = MenuDown;
 
@@ -123,6 +125,11 @@ void PrintMainMenu()
 
 void PrintNetworkSettings()
 {
+	Buttons[KEY_RIGHT].OnPress = RunSelected;
+	Buttons[KEY_LEFT].OnPress = PrintMainMenu; // Return to top
+	Buttons[KEY_UP].OnPress = MenuUp;
+	Buttons[KEY_DOWN].OnPress = MenuDown;
+
 	CurrentMenu = &NetworkSettingsMenu;
 	ActiveMenuItems = sizeof(NetworkSettingsMenu) / sizeof(MenuItem);
 	ActiveMenuItem = 1;
@@ -131,10 +138,40 @@ void PrintNetworkSettings()
 
 void PrintBackLightMenu()
 {
+	Buttons[KEY_RIGHT].OnPress = RunSelected;
+	Buttons[KEY_LEFT].OnPress = PrintMainMenu; // Return to top
+	Buttons[KEY_UP].OnPress = MenuUp;
+	Buttons[KEY_DOWN].OnPress = MenuDown;
+
 	CurrentMenu = &BackLightMenu;
 	ActiveMenuItems = sizeof(BackLightMenu) / sizeof(MenuItem);
 	ActiveMenuItem = 1;
 	DrawMenu();
+}
+
+void ShowTime()
+{
+	LCDclear();
+	LCDdrawstring(0, 0, (*CurrentMenu)[ActiveMenuItem - 1].Name);
+	LCDdrawline(0, 8, LCDWIDTH, 8, BLACK);
+
+	time_t rawtime;
+	struct tm *timeinfo;
+	char buffer[32];
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer, 32, "%d/%m/%Y", timeinfo);
+	LCDdrawstring(10, 18, buffer);
+	strftime(buffer, 32, "%H:%M:%S", timeinfo);
+	LCDdrawstring(15, 34, buffer);
+	LCDdisplay();
+
+	Buttons[KEY_RIGHT].OnPress = DoNothing;
+	Buttons[KEY_UP].OnPress = DoNothing;
+	Buttons[KEY_DOWN].OnPress = DoNothing;
+	Buttons[KEY_LEFT].OnPress = PrintMainMenu; // Return to previous menu and reset button controls
 }
 
 void TurnBacklightOn()
@@ -160,8 +197,8 @@ int main()
 	printf("Program started...\n");
 	signal(SIGINT, intHandler);
 
-	if (wiringPiSetup () == -1)
-    	return 1;
+	if (wiringPiSetup() == -1)
+		return 1;
 
 	//wiringPiSetupSys();
 
@@ -171,8 +208,8 @@ int main()
 
 	// LCD Init: CLK, DIN, DC, CS, RST, Contrast (Max: 127)
 	LCDInit(LCD_PINS, LCD_CONTRAST);
-	LCDshowLogo(FoxLogo);
-	// TODO: Show until system fully started (Boot logo)
+	LCDshowLogo(BootLogo);
+	// TODO: Wait until system fully started (Boot logo)
 	delay(1000);
 
 	// Init buttons
@@ -188,15 +225,18 @@ int main()
 	// or
 	//MainMenu[1].Run();
 
-	while (keepRunning) {
+	while (keepRunning)
+	{
 		for (int i = 0; i < MAX_BTN; i++)
 		{
 			// LOW = Pressed (0), HIGH = unpressed (1)
-			if (digitalRead(Buttons[i].pin) == LOW) {
+			if (digitalRead(Buttons[i].pin) == LOW)
+			{
 				// Wait some time for debounce
 				delay(50);
 				// Read again
-				while (digitalRead(Buttons[i].pin) == LOW && keepRunning) {
+				while (digitalRead(Buttons[i].pin) == LOW && keepRunning)
+				{
 					// Still pressed, wait for unhold
 				}
 
